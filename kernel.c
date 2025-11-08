c23d36e0cea6499fc0c04b730c5326465eff55d0
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-11-08 12:37:37 +0000

    Add files via upload

---
+++ kernel.c
@@ -0,0 +1,16 @@
+#include "base.h"
+
+void kernel_main()
+{
+        int n=0;
+        int xx;
+        char Yellow=14;
+        char Black=0;
+	cls(Yellow);
+        for(xx=0;xx<50;xx=xx+10)window(xx,xx,100,100,"hello world",Yellow-8);
+        while(1){}
+	
+	
+}
+
+
